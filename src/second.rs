d294e380152473e5f580f7c3eaa6cfd5c12fca77
John P Mayer Jr <john.p.mayer.jr@gmail.com>
2015-07-27 17:59:16 -0400

    IntoIter (value) and Iter (borrowed ref)

--- src/second.rs
+++ src/second.rs
@@ -87,3 +87,75 @@ impl<T> Drop for List<T> {
     }
 }
 
+// IntoIter: iterator when we OWN the linked list
+
+// Just a quick wrapper, new syntax!
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+#[test]
+fn into_iter() {
+    let mut list = List::new();
+    list.push(1); list.push(2); list.push(3);
+
+    let mut iter = list.into_iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+}
+
+// Iter: iterator when we only have a borrow reference to the list
+
+// the lifetime of the iterator is the lifetime of the node it might hold
+// Also need to say the inner type has the same lifetime?
+pub struct Iter<'a, T: 'a> {
+    next: Option<&'a Node<T>>,
+}
+
+// List doesn't have a lifetime, only the actual creation of the iter
+impl<T> List<T> {
+    // the iter has a lifetime, it comes from the list
+    pub fn iter(&self) -> Iter<T> {
+        // need a ref to the boxed node
+        Iter { next: self.head.as_ref().map(|boxed_node| &**boxed_node) }
+    }
+}
+
+// but we do need it here, as Iter does have a lifetime
+impl<'a, T> Iterator for Iter<'a, T> {
+    // lifetime of each element is same as the iterator
+    type Item = &'a T;
+    
+    // everything else just works (yiss)
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            // need a ref to the boxed node
+            // Exercise - expand the &** crap
+            self.next = node.next.as_ref().map(|boxed_node| &**boxed_node);
+            &node.elem
+        })
+    }
+}
+
+#[test]
+fn iter() {
+    let mut list = List::new();
+    list.push(1); list.push(2); list.push(3);
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
+}
